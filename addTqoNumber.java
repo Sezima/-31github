8df8800fd40ecb914449b93183cea61cf2cc4e10
sezim <osmsezimka@gmail.com>
2023-11-14 21:29:52 +0600

    завершить задачу позже

---
+++ addTqoNumber.java
@@ -0,0 +1,41 @@
+public class addTqoNumber {
+    //You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    //
+    //You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    //
+    //
+    //
+    //Example 1:
+    //
+    //
+    //Input: l1 = [2,4,3], l2 = [5,6,4]
+    //Output: [7,0,8]
+    //Explanation: 342 + 465 = 807.
+    //Example 2:
+    //
+    //Input: l1 = [0], l2 = [0]
+    //Output: [0]
+    //Example 3:
+    //
+    //Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    //Output: [8,9,9,9,0,0,0,1]
+    //
+
+    public static void main(String[] args) {
+        int[][] num = {{2, 4, 3}, {5, 6, 4}}; // Исправлено: добавлены фигурные скобки
+        String a = "";
+        String b = "";
+        for (int j = 0; j < num[0].length; j++){
+                a +=  num[0][j];
+            }
+
+        for (int j = 0; j < num[1].length; j++){
+            b +=  num[1][j];
+        }
+        int summa = Integer.parseInt(a) + Integer.parseInt(b);
+
+        System.out.println("Сумма: " + summa);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
